8dc615f081416f506c7e915352ada8af975745ce
Ngo Duc Canh <ngoduccanh19@gmail.com>
2024-09-26 15:11:28 +0700

    feat: add interface specialty home page

--- src/modules/home/constants/index.ts
+++ src/modules/home/constants/index.ts
@@ -28,3 +28,209 @@ export const CAROUSEL_HOMEPAGE = [
     label: 'Thanh toán viện phí'
   }
 ]
+
+export const SPECIALTY = [
+  {
+    icon: '/images/bac_si_gia_dinh.png',
+    label: 'Bác sĩ Gia Đình'
+  },
+  {
+    icon: '/images/tieu_hoa_gan_mat.png',
+    label: 'Tiêu hóa gan mật'
+  },
+  {
+    icon: '/images/tong_quat.png',
+    label: 'Nội tổng quát'
+  },
+  {
+    icon: '/images/noi_tiet.png',
+    label: 'Nội Tiết'
+  },
+  {
+    icon: '/images/da_lieu.png',
+    label: 'Da liễu'
+  },
+  {
+    icon: '/images/tim_mach.png',
+    label: 'Nội Tim Mạch'
+  },
+  {
+    icon: '/images/than_kinh.png',
+    label: 'Nội Thần Kinh'
+  },
+  {
+    icon: '/images/noi_co_xuong_khop.png',
+    label: 'Nội Cơ Xương Khớp'
+  },
+  {
+    icon: '/images/tai_mui_hong.png',
+    label: 'Tai Mũi Họng'
+  },
+  {
+    icon: '/images/mat.png',
+    label: 'Mắt'
+  },
+  {
+    icon: '/images/tieu_hoa.png',
+    label: 'Nội Tiêu Hoá'
+  },
+  {
+    icon: '/images/gan.png',
+    label: 'Nội Truyền Nhiễm'
+  },
+  {
+    icon: '/images/ho_hap.png',
+    label: 'Nội Hô Hấp'
+  },
+  {
+    icon: '/images/tiet_nieu.png',
+    label: 'Nội Tiết Niệu'
+  },
+  {
+    icon: '/images/xuong_khop_chinh_hinh.png',
+    label: 'Ngoại Cơ Xương Khớp'
+  },
+  {
+    icon: '/images/san_phu_khoa.png',
+    label: 'Sản - Phụ Khoa'
+  }
+]
+
+export const SPECIALTY_MORE = [
+  {
+    icon: '/images/bac_si_gia_dinh.png',
+    label: 'Bác sĩ Gia Đình'
+  },
+  {
+    icon: '/images/tieu_hoa_gan_mat.png',
+    label: 'Tiêu hóa gan mật'
+  },
+  {
+    icon: '/images/tong_quat.png',
+    label: 'Nội tổng quát'
+  },
+  {
+    icon: '/images/noi_tiet.png',
+    label: 'Nội Tiết'
+  },
+  {
+    icon: '/images/da_lieu.png',
+    label: 'Da liễu'
+  },
+  {
+    icon: '/images/tim_mach.png',
+    label: 'Nội Tim Mạch'
+  },
+  {
+    icon: '/images/than_kinh.png',
+    label: 'Nội Thần Kinh'
+  },
+  {
+    icon: '/images/noi_co_xuong_khop.png',
+    label: 'Nội Cơ Xương Khớp'
+  },
+  {
+    icon: '/images/tai_mui_hong.png',
+    label: 'Tai Mũi Họng'
+  },
+  {
+    icon: '/images/mat.png',
+    label: 'Mắt'
+  },
+  {
+    icon: '/images/tieu_hoa.png',
+    label: 'Nội Tiêu Hoá'
+  },
+  {
+    icon: '/images/gan.png',
+    label: 'Nội Truyền Nhiễm'
+  },
+  {
+    icon: '/images/ho_hap.png',
+    label: 'Nội Hô Hấp'
+  },
+  {
+    icon: '/images/tiet_nieu.png',
+    label: 'Nội Tiết Niệu'
+  },
+  {
+    icon: '/images/xuong_khop_chinh_hinh.png',
+    label: 'Ngoại Cơ Xương Khớp'
+  },
+  {
+    icon: '/images/san_phu_khoa.png',
+    label: 'Sản - Phụ Khoa'
+  },
+  {
+    icon: '/images/tieu_hoa.png',
+    label: 'Ngoại Tiêu Hoá'
+  },
+  {
+    icon: '/images/tiet_nieu.png',
+    label: 'Ngoại Tiết Niệu'
+  },
+  {
+    icon: '/images/tam_ly.png',
+    label: 'Tâm Lý'
+  },
+  {
+    icon: '/images/tam_than_kinh.png',
+    label: 'Tâm Thần Kinh'
+  },
+  {
+    icon: '/images/ho_hap.png',
+    label: 'Ngoại Hô Hấp'
+  },
+  {
+    icon: '/images/ngoai_than_kinh.png',
+    label: 'Ngoại Thần Kinh'
+  },
+  {
+    icon: '/images/long_nguc_mach_mau.png',
+    label: 'Lồng Ngực - Mạch Máu'
+  },
+  {
+    icon: '/images/dinh_duong.png',
+    label: 'Dinh Dưỡng'
+  },
+  {
+    icon: '/images/tham_my_da.png',
+    label: 'Thẩm mỹ da'
+  },
+  {
+    icon: '/images/noi_tong_quat.png',
+    label: 'Ngoại Tổng Quát'
+  },
+  {
+    icon: '/images/di_ung_mien_dich_lam_sang.png',
+    label: 'Dị Ứng - Miễn Dịch Lâm Sàng'
+  },
+  {
+    icon: '/images/rang_ham_mat.png',
+    label: 'Răng Hàm Mặt'
+  },
+  {
+    icon: '/images/chan_thuong_chinh_hinh.png',
+    label: 'Chấn Thương Chỉnh Hình'
+  },
+  {
+    icon: '/images/khoa_san.png',
+    label: 'Vô Sinh - Hiếm Muộn'
+  },
+  {
+    icon: '/images/nam_khoa.png',
+    label: 'Nam Khoa'
+  },
+  {
+    icon: '/images/phuc_hoi_chuc_nang.png',
+    label: 'Vật Lý Trị Liệu - Phục Hồi Chức Năng'
+  },
+  {
+    icon: '/images/bac_si_gia_dinh.png',
+    label: 'Y Học Cổ Truyền'
+  },
+  {
+    icon: '/images/tao_hinh_tham_my.png',
+    label: 'Tạo hình thẩm mỹ'
+  }
+]
--- src/modules/home/view/HomeView.vue
+++ src/modules/home/view/HomeView.vue
@@ -7,7 +7,7 @@
       <BaseInput v-model="search" placeholder="Tìm kiếm gói khám" class="input-search" :show-icon="true" />
       <p class="mt-5 text-center text-xl">Đặt khám nhanh - Lấy số thứ tự trực tuyến - Tư vấn sức khỏe từ xa</p>
     </div>
-    <div class="absolute -bottom-16 flex w-full items-center justify-center space-x-6">
+    <div class="carousel">
       <template v-for="(item, index) in CAROUSEL_HOMEPAGE" :key="index">
         <div class="carousel-item">
           <img :src="item.icon" alt="" class="mx-auto w-14" />
@@ -16,13 +16,32 @@
       </template>
     </div>
   </div>
-  <div class="mt-28"></div>
+  <!-- Chuyên khoa -->
+  <div class="mx-auto my-28 max-w-7xl">
+    <p class="text-center text-[28px] font-bold">Chuyên khoa</p>
+    <div class="my-8 grid grid-cols-8 gap-x-12 gap-y-8">
+      <template v-for="(item, index) in showMore ? SPECIALTY_MORE : SPECIALTY" :key="index">
+        <div class="cursor-pointer">
+          <img :src="item.icon" alt="" class="mx-auto w-20" />
+          <p class="mt-2 text-center text-[20px]">{{ item.label }}</p>
+        </div>
+      </template>
+    </div>
+    <div class="flex items-center justify-center" @click="showMore = !showMore">
+      <div class="show-more">
+        <p v-if="showMore">Thu gọn</p>
+        <p v-else>Xem tất cả</p>
+        <BaseIcon :name="showMore ? 'direction-up' : 'direction-down'" class="ml-2" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { CAROUSEL_HOMEPAGE } from '../constants/index'
+import { CAROUSEL_HOMEPAGE, SPECIALTY, SPECIALTY_MORE } from '../constants/index'
 
 const search = ref<string>('')
+const showMore = ref<boolean>(false)
 </script>
 
 <style scoped lang="scss">
@@ -34,9 +53,18 @@ const search = ref<string>('')
     width: 700px;
   }
 }
-.carousel-item {
-  box-shadow: 4px 8px 30px 0 #b1c4da59;
-  @apply h-[150px] w-[150px] cursor-pointer rounded-2xl border border-solid bg-white p-4;
+.carousel {
+  @apply absolute -bottom-16 flex w-full items-center justify-center space-x-6;
+  .carousel-item {
+    box-shadow: 4px 8px 30px 0 #b1c4da59;
+    @apply h-[150px] w-[150px] cursor-pointer rounded-2xl border border-solid bg-white p-4;
+    &:hover {
+      @apply border-primary;
+    }
+  }
+}
+.show-more {
+  @apply flex cursor-pointer items-center justify-center rounded-lg border border-solid px-3 py-2 text-[20px] text-primary;
   &:hover {
     @apply border-primary;
   }
