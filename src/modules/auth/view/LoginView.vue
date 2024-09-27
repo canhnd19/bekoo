5c1c4d080cdf3f0fee4512406d4bdefc685c83b3
Ngo Duc Canh <ngoduccanh19@gmail.com>
2024-09-27 14:11:29 +0700

    chore: rename tag

--- src/modules/auth/view/LoginView.vue
+++ src/modules/auth/view/LoginView.vue
@@ -3,23 +3,23 @@
     <div class="mx-auto">
       <img src="/images/header_logo.png" class="mx-auto my-7" />
       <p class="mb-[18px] text-center">Vui lòng đăng nhập để tiếp tục</p>
-      <el-form
+      <ElForm
         ref="formRef"
         style="max-width: 600px"
         :model="loginValidateForm"
         label-width="auto"
         class="demo-ruleForm"
       >
-        <el-form-item prop="phoneNumber" :rules="{ required: true, message: 'Vui lòng nhập số điện thoại!' }">
-          <el-input
+        <ElFormItem prop="phoneNumber" :rules="{ required: true, message: 'Vui lòng nhập số điện thoại!' }">
+          <ElInput
             v-model.number="loginValidateForm.phoneNumber"
             autocomplete="off"
             style="width: 394px; height: 50px"
             placeholder="Số điện thoại"
           />
-        </el-form-item>
-        <el-form-item prop="password" :rules="{ required: true, message: 'Vui lòng nhập mật khẩu!' }">
-          <el-input
+        </ElFormItem>
+        <ElFormItem prop="password" :rules="{ required: true, message: 'Vui lòng nhập mật khẩu!' }">
+          <ElInput
             v-model.number="loginValidateForm.password"
             autocomplete="off"
             style="width: 394px; height: 50px"
@@ -27,8 +27,8 @@
             placeholder="Mật khẩu"
             show-password
           />
-        </el-form-item>
-      </el-form>
+        </ElFormItem>
+      </ElForm>
       <BaseButton :disabled="disabled" size="large">Đăng nhập</BaseButton>
       <div class="my-10 border-b border-solid border-[#cacaca]"></div>
       <p class="text-center">
@@ -36,7 +36,6 @@
         <span class="cursor-pointer text-primary">Đăng ký ngay</span>
       </p>
     </div>
-
     <div class="banner-login">
       <div class="style_shape__1HA08"></div>
     </div>
