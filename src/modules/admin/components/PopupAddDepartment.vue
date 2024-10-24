004d46247e09790a5352f9aa42217efed74d23a4
Ngọ Đức Cảnh <ngoduccanh19@gmail.com>
2024-10-24 11:49:20 +0700

    feat: update api create department

--- src/modules/admin/components/PopupAddDepartment.vue
+++ src/modules/admin/components/PopupAddDepartment.vue
@@ -2,6 +2,22 @@
   <BasePopup name="popup-add-department" width="700">
     <template #title> Add department </template>
     <div class="space-y-3">
+      <div class="flex justify-center">
+        <div class="relative w-40">
+          <img :src="department.image" alt="" class="h-40 w-full rounded-full object-cover" />
+          <ElUpload
+            :show-file-list="false"
+            :auto-upload="false"
+            accept=".png, .jpg, .webp, .jpeg"
+            :on-change="handleSelectFile"
+            class="absolute bottom-4 right-1"
+          >
+            <div class="border-border-table rounded-full border border-solid bg-white p-2">
+              <BaseIcon name="edit-avatar" />
+            </div>
+          </ElUpload>
+        </div>
+      </div>
       <div class="space-y-2">
         <p class="text-label">Tên chuyên khoa</p>
         <ElInput
@@ -40,17 +56,27 @@ import { apiDepartment } from '@/services'
 import { useBaseStore } from '@/stores/base'
 
 const { setOpenPopup } = useBaseStore()
-
+const file = ref<Record<string, any>>({})
 const isLoading = ref<boolean>(false)
 const department = ref<Record<string, any>>({
   name: '',
-  description: ''
+  description: '',
+  image: ''
 })
+
+const handleSelectFile = async (_file: Record<string, any>) => {
+  file.value = _file
+  department.value.image = URL.createObjectURL(file.value.raw)
+}
 
 const handleCreateDepartment = async () => {
   try {
     isLoading.value = true
-    const rs = await apiDepartment.createdDepartment({ ...department.value })
+    const formData = new FormData()
+    formData.append('image', file.value.raw)
+    formData.append('name', department.value.name)
+    formData.append('description', department.value.description)
+    const rs = await apiDepartment.createdDepartment(formData)
     ElMessage.success(rs.message)
     setOpenPopup('popup-add-department', false)
     isLoading.value = false
