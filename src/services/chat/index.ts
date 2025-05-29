95c04db99a1566e7e47575f35a3a797fa22aa917
Ngọ Đức Cảnh <ngoduccanh19@gmail.com>
2025-05-29 09:42:39 +0700

    feat: comment out unused imports and getListUserChat method in ChatService

--- src/services/chat/index.ts
+++ src/services/chat/index.ts
@@ -1,18 +1,17 @@
-import request from '@/plugin/request'
+// import request from '@/plugin/request'
 
-import type { IMessage } from '@/types/message.types'
-import type { IResponse } from '@/types/response.types'
+// import type { IResponse } from '@/types/response.types'
 
 export default class ChatService {
   prefix = 'chat'
-  async getListUserChat(params: Record<string, any>): Promise<IResponse<IMessage[]>> {
-    try {
-      const rs = await request.get(`${this.prefix}/list-user`, {
-        params: params
-      })
-      return Promise.resolve(rs.data)
-    } catch (error) {
-      return Promise.reject(error)
-    }
-  }
+  // async getListUserChat(params: Record<string, any>): Promise<IResponse<IMessage[]>> {
+  //   try {
+  //     const rs = await request.get(`${this.prefix}/list-user`, {
+  //       params: params
+  //     })
+  //     return Promise.resolve(rs.data)
+  //   } catch (error) {
+  //     return Promise.reject(error)
+  //   }
+  // }
 }
