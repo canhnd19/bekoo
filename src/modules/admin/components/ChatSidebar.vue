7dad709274f3c449f4838343c1628a475223de90
Ngọ Đức Cảnh <ngoduccanh19@gmail.com>
2025-06-14 11:22:15 +0700

    fix: update unread message badge styling in ChatSidebar component

--- src/modules/admin/components/ChatSidebar.vue
+++ src/modules/admin/components/ChatSidebar.vue
@@ -58,7 +58,7 @@
               <span class="time">{{ chat.time }} </span>
               <span
                 v-if="chat.totalUnreadMessages > 1"
-                class="absolute right-0 top-4 flex h-6 w-6 items-center justify-center rounded-full bg-[#007bff] text-[#003553] text-white"
+                class="absolute right-0 top-4 flex h-6 w-6 items-center justify-center rounded-full bg-[#007bff] text-white"
                 >{{ chat.totalUnreadMessages }}</span
               >
             </p>
